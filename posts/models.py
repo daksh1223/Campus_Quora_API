d82dbe7c4fb8422801dceb3bd9cd56a195017eb4
Your Name <divysharma160@gmail.com>
2021-06-06 00:00:30 +0530

    Views done

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime 
+import datetime
 # Create your models here.
+
 
 class Community(models.Model):
     name = models.TextField(unique=True)
     users = models.ManyToManyField(User)
+
     def __str__(self):
         return f"id:{self.id} {self.name}"
 
@@ -15,24 +17,30 @@ class Question(models.Model):
     content = models.TextField()
     date = models.DateField(default=datetime.date.today)
     communities = models.ManyToManyField(Community)
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     class Meta:
-        unique_together = ('title','content',)
+        unique_together = ('title', 'content',)
+
     def __str__(self):
         return f"id:{self.id} {self.title}"
+
 
 class Answer(models.Model):
     content = models.TextField()
     date = models.DateField(default=datetime.date.today)
-    question = models.ForeignKey(Question,on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"id:{self.id} {self.content[:30]}"
+
 
 class Comment(models.Model):
     content = models.TextField()
     date = models.DateField(default=datetime.date.today)
-    answer = models.ForeignKey(Answer,on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"id:{self.id} {self.content[:30]}"
--- posts/views.py
+++ posts/views.py
@@ -1,41 +1,180 @@
+from django.http import response
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Community,Question,Answer,Comment
+from .models import Community, Question, Answer, Comment
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 import datetime
+import json
 from rest_framework import status
 import django.db.utils
 
 # Create your views here.
 
+
+class ParticularQuestionView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, question_id):
+        try:
+            question = Question.objects.get(pk=question_id)
+            response = QuestionSerializer(question)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"question with id : {question_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, question_id):
+        try:
+            question = Question.objects.get(pk=question_id)
+            if question.user == request.user:
+                question.delete()
+                return Response({"message": "succesfully deleted"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response({"error": f"question with id : {question_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, question_id):
+        try:
+            user = request.user
+            question = Question.objects.get(pk=question_id)
+            if question.user == user:
+                response = QuestionSerializer(question)
+                question.title = request.data['title']
+                question.content = request.data['content']
+                comm_list = []
+                for x in request.data["communities"]:
+                    # print("----------------",x)
+                    print(Community.objects.all())
+                    community = Community.objects.get(name=x)
+                    if community.users.filter(username=user.username).exists():
+                        comm_list.append(community)
+                    else:
+                        return Response({"error": f"{user} not present in {community}"}, status=status.HTTP_401_UNAUTHORIZED)
+                question.communities.add(*comm_list)
+                question.save()
+                return Response(response.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response({"error": f"question with id : {question_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ParticularAnswerView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, answer_id):
+        try:
+            answer = Answer.objects.get(pk=answer_id)
+            response = AnswerSerializer(answer)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"answer with id : {answer_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, answer_id):
+
+        try:
+
+            answer = Answer.objects.get(pk=answer_id)
+            if request.user == answer.user:
+                answer.delete()
+                return Response({"message": "succesfully deleted"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        except:
+            return Response({"error": f"answer with id : {answer_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, answer_id):
+
+        try:
+
+            answer = Answer.objects.get(pk=answer_id)
+            if request.user == answer.user:
+                answer.content = request.data['content']
+                answer.date = datetime.date.today()
+                answer.save()
+                response = AnswerSerializer(answer)
+                return Response(response.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        except:
+            return Response({"error": f"answer with id : {answer_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ParticularCommentView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, reuqest, comment_id):
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+            response = CommentSerializer(comment)
+            return Response(response.data,status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"comment with id : {comment_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, comment_id):
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+            if comment.user==request.user:
+             comment.delete()
+             return Response({"message": "succesfully deleted"}, status=status.HTTP_200_OK)
+            else :
+             return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+             return Response({"error": f"comment with id : {comment_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, comment_id):
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+            if comment.user == request.user:
+             comment.content=request.data['content']
+             comment.date = datetime.date.today()
+             comment.save()
+             response=CommentSerializer(comment)
+             return Response(response.data, status=status.HTTP_200_OK)
+            else:
+             return Response({"error": "Not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response({"error": f"comment with id : {comment_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ParticularCommunityView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, community_id):
+        try:
+            community = Community.objects.get(pk=community_id)
+            questions = Question.objects.filter(communities=community)
+            response = QuestionSerializer(questions, many=True)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"Community with id : {community_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class QuestionView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def get(self,request):
-        return Response()
 
-    def post(self,request):
+    def post(self, request):
         user = request.user
         try:
-            # print(request.data["communities"])
-            # print(user.community_set.all())
             question = Question(
-                title = request.data['title'],
-                content = request.data['content'],
-                date =  datetime.date.today(),
-                user = user,
+                title=request.data['title'],
+                content=request.data['content'],
+                date=datetime.date.today(),
+                user=user,
             )
             comm_list = []
             for x in request.data["communities"]:
-                community = Community.objects.get(name= x)
-                print(community)
-                print(community.users.all())
-                if community.users.filter(username = user.username).exists():
+                print(Community.objects.all())
+                community = Community.objects.get(name=x)
+                if community.users.filter(username=user.username).exists():
                     comm_list.append(community)
                 else:
-                    return Response({"error": f"{user} not present in {community}"}, status= status.HTTP_401_UNAUTHORIZED)
+                    return Response({"error": f"{user} not present in {community}"}, status=status.HTTP_401_UNAUTHORIZED)
             question.save()
             try:
                 question.communities.add(*comm_list)
@@ -45,79 +184,89 @@ class QuestionView(APIView):
                 question.delete()
                 return Response({"error": "Unable to add communities to question!!"}, status=status.HTTP_400_BAD_REQUEST)
         except KeyError as e:
-            print("Exception: ",type(e),e)
+            # print("Exception: ", type(e), e)
             return Response({"error": "Incomplete data!!"}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            print("Exception: ",type(e),e)
+            # print("Exception: ", type(e), e)
             return Response({"error": "Question already exists or Server error!!"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AnswerView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def get(self,request,question_id):
-        return Response()
-    def post(self,request,question_id):
+    def get(self, request, question_id):
         try:
             question = Question.objects.get(pk=question_id)
-            answer,is_created = Answer.objects.get_or_create(
+            print(question)
+            answer = Answer.objects.filter(question=question)
+            response = AnswerSerializer(answer, many=True)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"question with id : {question_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, question_id):
+        try:
+            question = Question.objects.get(pk=question_id)
+            answer, is_created = Answer.objects.get_or_create(
                 content=request.data['content'],
-                date = datetime.date.today(),
-                question = question,
-                user = request.user,
+                date=datetime.date.today(),
+                question=question,
+                user=request.user,
             )
             serializer = AnswerSerializer(answer)
-            print(answer,is_created)
+            print(answer, is_created)
             if not is_created:
-                return Response({"error":"Already exists","data":serializer.data},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Already exists", "data": serializer.data}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         except KeyError as e:
-            print("Exception: ",type(e),e)
+            print("Exception: ", type(e), e)
             return Response({"error": "Incomplete data!!"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": ""}, status=status.HTTP_400_BAD_REQUEST)
+        # return Response({"error": ""}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def get(self,request,question_id):
-        return Response()
-    def post(self,request,question_id,answer_id):
+    def get(self, request, answer_id):
         try:
             answer = Answer.objects.get(pk=answer_id)
-            comment,is_created = Comment.objects.get_or_create(
+            comments = Comment.objects.filter(answer=answer)
+            response = CommentSerializer(comments, many=True)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": f"Answer with id : {answer_id} does not exist "}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request,  answer_id):
+        try:
+            answer = Answer.objects.get(pk=answer_id)
+            comment, is_created = Comment.objects.get_or_create(
                 content=request.data['content'],
-                date = datetime.date.today(),
-                answer = answer,
-                user = request.user,
+                date=datetime.date.today(),
+                answer=answer,
+                user=request.user,
             )
             serializer = CommentSerializer(comment)
-            print(comment,is_created)
+            print(comment, is_created)
             if not is_created:
-                return Response({"error":"Already exists","data":serializer.data},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Already exists", "data": serializer.data}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         except KeyError as e:
-            print("Exception: ",type(e),e)
+            print("Exception: ", type(e), e)
             return Response({"error": "Incomplete data!!"}, status=status.HTTP_400_BAD_REQUEST)
         return Response({"error": ""}, status=status.HTTP_400_BAD_REQUEST)
 
-# class TagList(APIView):
-#     #  permission_classes = (IsAuthenticated,)  
 
-#      def get(self,request):      
-#         tags = Tag.objects.all()
-#         serializer = TagSerializer(tags,many=True)
-#         print("print -",serializer,type(serializer))
-#         return Response(serializer.data)
+class CommunityView(APIView):
+    permission_classes = (IsAuthenticated,)
 
-# class TagDetail(APIView):
-#     def get(self,request,tag_id):
-#         queryset = Tag.objects.get(pk=tag_id).question_set.all()
-#         print(type(queryset),queryset)
-#         # questions = Question.objects.filter(tag__id = tag_id)
-#         # print(questions)
-#         return Response()
+    def get(self, request):
+        try:
+            communities = Community.objects.all()
+            response = CommunitySerializer(communities, many=True)
+            return Response(response.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": "Server error"}, status=status.HTTP_400_BAD_REQUEST)
